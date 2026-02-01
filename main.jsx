e3c1747f38560cc5ab272625e9bd5704f82f0373
ZakIIDev <demighazakaria@gmail.com>
2026-02-01 03:10:09 +0100

    fix: add missing entry points (index.html, main.jsx) and scripts

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BountyApp from './BountyApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BountyApp />
+  </React.StrictMode>,
+)
